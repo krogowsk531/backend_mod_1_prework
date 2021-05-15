b11e66b405e683c2ce12a0cab21b4277afd156fd
Katy St Sauveur <katyrogowski@yahoo.com>
2021-05-15 00:35:22 -0700

    First exercise complete regarding conditionals.

---
+++ section2/exercises/ex1.rb
@@ -0,0 +1,44 @@
+# Statements under if will be executed only when this condition is true otherwise the statements under else will be executed. 
+# Code under the if needs to be indented two spaces for readibility and user empathy
+# If it isn't indented it will still run as expected, but it makes it difficult for others to read your code 
+# If you change the initial values for people, cats and dogs what is printed in the terminal will change based on the conditional coded
+# The code x += 1 is the same as doing x = x + 1 but involves less typing
+
+
+# people = 20
+# cats = 30
+# dogs = 15
+
+people = 10
+cats = 50
+dogs = 25
+
+if people < cats
+  puts "Too many cats! The world is doomed!"
+end
+
+if people > cats
+  puts "Not many cats!  The world is saved!"
+end
+
+if people > dogs
+  puts "The world is dry!"
+end
+
+dogs += 5
+
+if people >= dogs
+  puts "People are greater than or equal to dogs."
+end
+
+if people <= dogs
+  puts "People are less than or equal to dogs."
+end
+
+if people == dogs
+  puts "People are dogs."
+end
+
+if people != dogs
+  puts "People are not dogs."
+end
